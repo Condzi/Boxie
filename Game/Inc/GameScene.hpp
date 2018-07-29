651f438f064019c720ee929690a4037f45ced5e9
Condzi <Condzi@users.noreply.github.com>
2018-07-29 18:52:00 +0200

    put test player representation on screen

--- Game/Inc/GameScene.hpp
+++ Game/Inc/GameScene.hpp
@@ -10,12 +10,19 @@ class GameScene final :
 {
 public:
 	con::BitmapText text;
+	con::RectangleShape testShape;
+
 	void onPush() override
 	{
 		tag = "Game";
-		con::Global.Assets.BitmapFonts.load( "data/vincent.png", "vincent" );
-		// @ToDo: Fix this so we don't have to const_cast to set the glyph size.
-		con::Global.Assets.BitmapFonts.get( "vincent" ).setGlyphSize( { 8,8 } );
-		text = con::BitmapText( "Hello, World!", con::Global.Assets.BitmapFonts.get( "vincent" ) );
+		text = con::BitmapText( "HELLO, I'M BOXIE", con::Global.Assets.BitmapFonts.getDefault(), { 0,0 }, sf::Color( 200,200,200 ) );
+		testShape.setPosition( 1, 8 );
+		testShape.setSize( { 2+2, 2+2 } );
+		testShape.setFillColor( sf::Color( 175, 50, 50 ) );
+
+		sf::View view = con::Global.GameWindow.getDefaultView();
+		view.setCenter( 64, 48 );
+		view.zoom( 1/8.f);
+		con::Global.GameWindow.setView( view );
 	}
 };
