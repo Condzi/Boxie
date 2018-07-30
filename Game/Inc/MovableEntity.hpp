687b45cb684a44094b03f5ca5272cb58ea97d2c4
Condzi <Condzi@users.noreply.github.com>
2018-07-30 09:54:44 +0200

    add MovableEntity class

---
+++ Game/Inc/MovableEntity.hpp
@@ -0,0 +1,21 @@
+/*
+	Conrad 'Condzi' Kubacki 2018
+	https://github.com/condzi
+*/
+
+#pragma once
+
+class MovableEntity :
+	public con::Entity
+{
+public:
+	sf::Time TransisionTime = sf::seconds( 0.3f );
+	enum class MovementDirection
+	{
+		None, Left, Right, Up, Down
+	} MoveDirection;
+	// Shouldn't be set manually. It determinates how fast should move entity to go to
+	// the tile.
+	Vec2f Velocity{};
+	class Tile* OccupiedTile = nullptr;
+};
